d50baf54e3fcd1f59467a69df9990fa05640aad6
vietanh112 <imnova1212@gmail.com>
2023-03-24 17:03:13 +0700

    refactor

--- kython/aes256ctr.py
+++ kython/aes256ctr.py
@@ -2,14 +2,14 @@ from ctypes import cdll, cast, create_string_buffer
 from ctypes import c_int, c_size_t, c_ubyte, POINTER
 import numpy as np
 import os
-
-aes256ctr_ref = cdll.LoadLibrary(os.path.abspath(os.path.join(
-    __file__,'..','..', 'ref/libpqcrystals_aes256ctr_ref.so')))
+from kython.c_func.aes256ctr_ref import (
+    C_aes256ctr_ctx, 
+    pqcrystals_kyber_aes256ctr_ref_prf           as prf,
+    pqcrystals_kyber_aes256ctr_ref_init          as init,
+    pqcrystals_kyber_aes256ctr_ref_squeezeblocks as squeezeblocks
+)
 
 def prf():
-    pqcrystals_kyber_aes256ctr_ref_prf = aes256ctr_ref.aes256ctr_ref
-    pqcrystals_kyber_aes256ctr_ref_prf.argtypes = [POINTER(c_ubyte), c_size_t, POINTER(c_ubyte), POINTER(c_ubyte)]
-    pqcrystals_kyber_aes256ctr_ref_prf.restype = None
     
     return
 
