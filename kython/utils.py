1fcac0cb7dd4d9afc4c7fe0ff5c92a1e3e23ddca
vietanh112 <imnova1212@gmail.com>
2023-03-24 11:03:06 +0700

    cvt via np.uint8

--- kython/utils.py
+++ kython/utils.py
@@ -1,6 +1,7 @@
 from ctypes import create_string_buffer
 from ctypes import c_uint8, c_char
 from typing import List
+import numpy as np
 
 
 def strBuffer_to_list(strBuff:'c_char*int'):
@@ -8,6 +9,13 @@ def strBuffer_to_list(strBuff:'c_char*int'):
 
 def list_to_strBuffer(xs:List[c_uint8]) -> 'c_char*int':
     return create_string_buffer(bytes(xs), len(xs))
+
+def strBuffer_to_npuint8(strBuff:'c_char*int'):
+    return np.frombuffer(strBuff, dtype=np.uint8)
+
+def npuint8_to_strBuffer(arr:np.uint8) -> 'c_char*int':
+    assert arr.dtype == np.uint8, "dtype must is `np.uint8`"
+    return create_string_buffer(arr.tobytes(), len(arr))
 
 # from ctypes import create_string_buffer
 # x0 = [1, 0, 2]
