f238232eb77d88982360ed1798fa9027c1af6232
vietanh112 <imnova1212@gmail.com>
2023-03-27 15:52:47 +0700

    draft

--- test_kem.py
+++ test_kem.py
@@ -1,17 +1,23 @@
 import numpy as np
-
+from ctypes import c_uint8, POINTER, cast, create_string_buffer
+from kython.utils import strBuffer_to_list, list_to_strBuffer, strBuffer_to_npuint8, npuint8_to_strBuffer
 from kython.pqc_kyber512_ref import (
     pqc_kyber512_ref_keypair as keypair,
     pqc_kyber512_ref_enc     as encode,
     pqc_kyber512_ref_dec     as decode
 )
-key_b = [72, 101, 108, 108, 111] * 6 + [72, 101]
+key_b = [72, 101, 108, 108, 111] * 6 + [2, 1]
 
 
 pk, sk = keypair()
 ct = encode(key_b, pk)
 key_a = decode(ct, sk)
 
-print(key_a)
-print(key_b)
+# print('\n')
+# print('py_pk = ', pk)
+# print('py_sk = ', sk)
+
+# print('ss1 =   ', ss1)
+# print('key_b = ', key_b)
+# print('key_a = ', key_a)
 # assert key_a == key_b
